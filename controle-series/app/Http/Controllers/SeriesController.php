aa14266b1c6951962985ed245f3bb089cdb07e16
x <karolina_bp@hotmail.com>
2022-09-19 14:38:51 -0300

    Criando a view em HTML de forma separada

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -6,20 +6,23 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(): void
+    public function index(Request $request)
     {
+        //transforma em json caso eu coloque o return
+        //o laravel pega o retorno e analisa a melhor forma de transformar em uma resposta
         $series = [
             'A',
             'B',
             'C'
         ];
 
-        $html = '<ul>';
-        foreach($series as $serie){
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
+        // return view('listar-series', [
+        //     //nome da variavel que será criada na view e conteúdo a ela atribuído
+        //     'series' => $series
+        // ]);
 
-        echo $html;
+        //compact pega o argumento que passamos como string e paga a variável do contexto com o mesmo nome
+        //e cria um array onde a chave é a string que defini e o valor é a variável do contexto
+        return view('listar-series', compact('series'));
     }
 }
---
+++ controle-series/resources/views/listar-series.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Series</title>
+</head>
+<body>
+    <h1>Séries</h1>
+    <ul>
+    <?php foreach ($series as $serie): ?>
+    <li><?= $serie; ?></li>
+    <?php endforeach; ?>
+    </ul>
+</body>
+</html>
