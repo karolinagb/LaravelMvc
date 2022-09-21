5ba7af27c4388556abce32d0af87f3e23a226afc
x <karolina_bp@hotmail.com>
2022-09-21 15:52:19 -0300

    formas de agrupar actions

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -15,10 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
+});
+
+//Porém se seguirmos o padrão de nomenclatura do Laravel, pode simplificar mais
+    //parametros = como o grupo de rotas inicia e o nome do controlador
+    //Dessa forma ele sabe que se for o metodo create, por exemplo, deve acessar series/create
+    //Para isso teriamos que alterar nossas rotas de portugues para ingles
+// Route::resource('/series', SeriesController::class)
+
+//agrupando as rotas por controlador
+Route::controller(SeriesController::class)->group(function (){
+    Route::get('/series', 'index');
+    Route::get('/series/criar', 'create');
+    Route::post('/series/salvar', 'store');
 });
 
 //array = primeira posição - classe controladora - segunda posição - método que deve ser executado
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+// Route::get('/series', [SeriesController::class, 'index']);
+// Route::get('/series/criar', [SeriesController::class, 'create']);
+// Route::post('/series/salvar', [SeriesController::class, 'store']);
